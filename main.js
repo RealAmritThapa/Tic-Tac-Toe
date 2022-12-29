d882862e7dcee509b72c094921e9a15d0a9ce04b
Amrit Thapa <realamritthapa@gmail.com>
2022-12-29 14:47:00 -0800

    fixed format and added color

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ const top_left = document
     let value = document.querySelector(".top-left").value;
     if (hot.key === "Enter") {
       top_arr[0].push(value);
+      let top_left = document.getElementById("top-left");
+      if (value == "x") {
+        top_left.style.backgroundColor = "#44a6c6";
+      } else {
+        top_left.style.backgroundColor = "#6bd284";
+      }
     }
   });
 
@@ -18,6 +24,12 @@ const top_middle = document
     let value = document.querySelector(".top-middle").value;
     if (hot.key === "Enter") {
       top_arr[1].push(value);
+      let top_mid = document.getElementById("top-middle");
+      if (value == "x") {
+        top_mid.style.backgroundColor = "#44a6c6";
+      } else {
+        top_mid.style.backgroundColor = "#6bd284";
+      }
     }
   });
 const top_right = document
@@ -26,6 +38,12 @@ const top_right = document
     let value = document.querySelector(".top-right").value;
     if (e.key === "Enter") {
       top_arr[2].push(value);
+      let top_right = document.getElementById("top-right");
+      if (value == "x") {
+        top_right.style.backgroundColor = "#44a6c6";
+      } else {
+        top_right.style.backgroundColor = "#6bd284";
+      }
     }
   });
 
@@ -35,6 +53,12 @@ const middle_left = document
     let value = document.querySelector(".middle-left").value;
     if (hot.key === "Enter") {
       mid_arr[0].push(value);
+      let middle_left = document.getElementById("middle-left");
+      if (value == "x") {
+        middle_left.style.backgroundColor = "#44a6c6";
+      } else {
+        middle_left.style.backgroundColor = "#6bd284";
+      }
     }
   });
 
@@ -44,6 +68,12 @@ const middle_middle = document
     let value = document.querySelector(".middle-middle").value;
     if (hot.key === "Enter") {
       mid_arr[1].push(value);
+      let middle_middle = document.getElementById("middle-middle");
+      if (value == "x") {
+        middle_middle.style.backgroundColor = "#44a6c6";
+      } else {
+        middle_middle.style.backgroundColor = "#6bd284";
+      }
     }
   });
 const middle_right = document
@@ -52,6 +82,12 @@ const middle_right = document
     let value = document.querySelector(".middle-right").value;
     if (e.key === "Enter") {
       mid_arr[2].push(value);
+      let middle_right = document.getElementById("middle-right");
+      if (value == "x") {
+        middle_right.style.backgroundColor = "#44a6c6";
+      } else {
+        middle_right.style.backgroundColor = "#6bd284";
+      }
     }
   });
 
@@ -61,6 +97,12 @@ const bottom_left = document
     let value = document.querySelector(".bottom-left").value;
     if (hot.key === "Enter") {
       bot_arr[0].push(value);
+      let bottom_left = document.getElementById("bottom-left");
+      if (value == "x") {
+        bottom_left.style.backgroundColor = "#44a6c6";
+      } else {
+        bottom_left.style.backgroundColor = "#6bd284";
+      }
     }
   });
 
@@ -70,6 +112,12 @@ const bottom_middle = document
     let value = document.querySelector(".bottom-middle").value;
     if (hot.key === "Enter") {
       bot_arr[1].push(value);
+      let bottom_middle = document.getElementById("bottom-middle");
+      if (value == "x") {
+        bottom_middle.style.backgroundColor = "#44a6c6";
+      } else {
+        bottom_middle.style.backgroundColor = "#6bd284";
+      }
     }
   });
 const bottom_right = document
@@ -78,6 +126,12 @@ const bottom_right = document
     let value = document.querySelector(".bottom-right").value;
     if (e.key === "Enter") {
       bot_arr[2].push(value);
+      let bottom_right = document.getElementById("bottom-right");
+      if (value == "x") {
+        bottom_right.style.backgroundColor = "#44a6c6";
+      } else {
+        bottom_right.style.backgroundColor = "#6bd284";
+      }
     }
   });
 
